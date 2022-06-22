d09cd8e23055e7ea99ccb1f9e4cfa2b11eddc241
Anderson Paschoalon <anderson.paschoalon@gmail.com>
2022-06-22 18:38:32 -0300

    adicionando template do gitpages para recriar site

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'jekyll'
+
+group :jekyll_plugins do
+  gem "jekyll-paginate"
+  gem "jekyll-feed"
+  gem "jekyll-sitemap"
+end
+
+gem "webrick", "~> 1.7"
--- en/cpptools.jsx
+++
@@ -1,6 +0,0 @@
-ReactDOM.render
-( 
-    <CardPhoto image="../img/img_snowtops.jpg" style={{width:'70%'}} text="Exemplo de texto" title="Exemplo de Titulo">
-    </CardPhoto>,
-    document.getElementById("card-photo-sample")
-);
